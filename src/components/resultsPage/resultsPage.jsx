1df52dd18d7af09a521e2b463d993f360daf6e2e
Iancu Victor - Iuliu <iancu27victor@gmail.com>
2022-06-30 12:58:29 +0300

    Fixed .env error

--- src/components/resultsPage/resultsPage.jsx
+++ src/components/resultsPage/resultsPage.jsx
@@ -9,7 +9,7 @@ function ResultsPage() {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+            'X-RapidAPI-Key': 'process.env.REACT_APP_API_KEY',
             'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
         }
     };
